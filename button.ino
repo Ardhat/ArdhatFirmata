d0bacd036817f29750890495971390e25d8a223b
ubiqio <ubqgit123>
2015-11-05 20:16:24 +0000

    update

--- button.ino
+++ button.ino
@@ -16,20 +16,17 @@ State Pushed()  {
   if (button.Timeout(DEBOUNCE)) { //wait for debounce
 
     if (map(analogRead(NAV), 0, 1023, 0, 12) > 3) {
-      button.Set(Open);
-
-      if (bitRead( PORTC, 3)) {    // if Pi power is turned off, turn it on and pulse LED
+      if (bitRead( PORTC, 3)) {   // if Pi power is turned off, turn it on, and status LED too
         pinMode(nPIPWR, OUTPUT);
         digitalWrite(nPIPWR, LOW);
-        led.Set(PulseStart);
-        
-      } else {           // else turn off PI and LED
+        digitalWrite(LED, HIGH);
+        led.Set(Idle); 
+      } else {                    // else turn off PI and pulse LED
         digitalWrite(nPIPWR, HIGH);
         pinMode(nPIPWR, INPUT_PULLUP);
-        digitalWrite(LED, LOW);
-        led.Set(Idle);
-        
+        led.Set(PulseStart);
       }
+    button.Set(Open); //update state machine
     }
   }
 }
